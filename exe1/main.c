cd4c4c0cd06aeb87419ee73115856c9bc20b7108
gabrielgsn <ggsnasser@gmail.com>
2025-03-09 22:26:47 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -6,6 +6,7 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int g_timer_0 = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
@@ -14,8 +15,18 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    return true; // keep repeating
+}
+
 int main() {
     stdio_init_all();
+
+    int timer_0_hz = 5;
+    repeating_timer_t timer_0;
+
+
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
@@ -26,10 +37,32 @@ int main() {
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
 
+    int rodando = 0;
+    int ligado = 0;
+
     while (true) {
 
-        if (flag_f_r) {
+        if(flag_f_r){
             flag_f_r = 0;
+            if(!rodando){
+                if(!add_repeating_timer_ms(500 / timer_0_hz, 
+                    timer_0_callback,
+                    NULL, 
+                    &timer_0)){
+                    printf("Failed to add timer\n");
+                } else(rodando = 1);
+            } else{
+                cancel_repeating_timer(&timer_0);
+                rodando = 0;
+                gpio_put(LED_PIN_R, 0);
+            }
         }
+
+        if(g_timer_0){
+            g_timer_0 = 0;
+            ligado = !ligado;
+            gpio_put(LED_PIN_R, ligado);
+        }
+        sleep_ms(10);
     }
 }
